0e8d4b99fda2c68f8d05aab59bc7b47192d74463
Alvin Felix <174065737+Alvin-Felix@users.noreply.github.com>
2025-12-24 20:59:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const products = [
   { id: 2, name: "Dosa", price: 40 }
 ];
 
+function getCart() {
+  return JSON.parse(localStorage.getItem("cart")) || {};
+}
+
+function saveCart(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function addToCart(id) {
+  const cart = getCart();
+  cart[id] = (cart[id] || 0) + 1;
+  saveCart(cart);
+  alert("Added to cart");
+}
+
 const productDiv = document.getElementById("products");
 
 if (productDiv) {
   products.forEach(p => {
     const div = document.createElement("div");
-    div.innerHTML = `${p.name} - ₹${p.price}`;
+    div.innerHTML = `
+      <p>${p.name} - ₹${p.price}</p>
+      <button onclick="addToCart(${p.id})">Add</button>
+    `;
     productDiv.appendChild(div);
   });
 }
